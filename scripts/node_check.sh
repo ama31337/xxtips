898953da8ed2106a4a39cde01ca9a9c095bf2019
ama31337 <ama31337@protonmail.com>
2020-12-05 11:31:03 +0700

    node check

---
+++ scripts/node_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
+XXNODE_PID=`ps -e | grep xxnetwork-node | grep defunct`
+
+if [ -z "$XXNODE_PID" ]; then
+    echo "`date` XX node working ..."
+else
+    echo "`date` ALARM, need to restart node"
+    sudo systemctl stop xxnetwork-node.service
+    sudo systemctl start xxnetwork-node.service
+    "${SCRIPT_DIR}/Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "XX node defunct and restarted now"  2>&1 > /dev/null
+fi
+
