b8f593410e49d585d81529f9074078ae4d7b0593
ama31337 <ama31337@protonmail.com>
2021-10-06 20:17:16 +0700

    commit

--- scripts/gate_check.sh
+++ scripts/gate_check.sh
@@ -8,6 +8,8 @@
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 node_check=$(sudo systemctl status xxnetwork-chain.service | grep running)
 gate_check=$(sudo systemctl status xxnetwork-gateway.service | grep running)
+node_check_ps=$(ps -e | grep xxnetwork-chain)
+gate_check_ps=$(ps -e | grep xxnetwork-gate)
 
 echo `date`
 
@@ -25,6 +27,24 @@ then
     echo "XX gate offline"
     sudo systemctl restart xxnetwork-gateway.service
     "${SCRIPT_DIR}/../Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "XX gateway was offline and restarted now"  2>&1 > /dev/null
+else
+    echo "online"
+fi
+
+if [[ -z "$node_check_ps" ]]
+then
+    echo "XX node process offline"
+    sudo systemctl restart xxnetwork-chain.service
+    "${SCRIPT_DIR}/../Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "XX node process was offline and restarted now"  2>&1 > /dev/null
+else
+    echo "online"
+fi
+
+if [[ -z "$gate_check_ps" ]]
+then
+    echo "XX gate offline"
+    sudo systemctl restart xxnetwork-gateway.service
+    "${SCRIPT_DIR}/../Send_msg_toTelBot.sh" "$HOSTNAME inform you:" "XX gateway process was offline and restarted now"  2>&1 > /dev/null
 else
     echo "online"
 fi
