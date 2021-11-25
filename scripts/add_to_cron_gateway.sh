5f34cba38b50477672e8d3071768e574e25bc5da
ama31337 <70890873+ama31337@users.noreply.github.com>
2021-11-25 10:49:17 +0700

    Update add_to_cron_gateway.sh

--- scripts/add_to_cron_gateway.sh
+++ scripts/add_to_cron_gateway.sh
@@ -3,6 +3,12 @@
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 mkdir -p $HOME/logs
 
+pkgs='jq'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y $pkgs
+fi
+
 crontab -l | 
 {
     echo "#crontab for $USER on $HOSTNAME"
