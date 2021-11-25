95b69a7f28e1c05fcebcb97f52f5f7b2bde1a403
ama31337 <70890873+ama31337@users.noreply.github.com>
2021-11-25 09:06:49 +0700

    Update add_to_cron_node.sh

--- scripts/add_to_cron_node.sh
+++ scripts/add_to_cron_node.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/logs
 crontab -l | 
 {
     echo "#crontab for $USER on $HOSTNAME"
-    echo "*/5 * * * * cd $SCRIPT_DIR && ./online_check_cmix.sh >> $HOME/logs/onlone_check.log "; 
+    echo "*/5 * * * * cd $SCRIPT_DIR && ./online_check.sh >> $HOME/logs/onlone_check.log "; 
     echo "* * * * * cd $SCRIPT_DIR && ./node_check.sh >> $HOME/logs/node_check.log ";
 } | crontab -
 
